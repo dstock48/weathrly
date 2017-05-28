66d97d9de2a2b2596e21acc54e805fc231f86eba
Dave W <dviper@gmail.com>
2017-05-28 16:39:25 -0600

    add AsideForecast component and styles

---
+++ lib/Weathrly/components/AsideForecast/AsideForecast.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './AsideForecast.css';
+// import icons from "../../../utils/weather-icons";
+
+function AsideForecast() {
+
+  // const cityState = `${props.city}, ${props.state}`;
+  // const date =
+  // const
+  const highLow = `${70}° / ${50}°`;
+
+  return(
+    <aside className='AsideForecast'>
+
+      <div className="top">
+        <h1 className="city">Denver, CO</h1>
+        <h2 className="date">Sun, May 28 2017</h2>
+      </div>
+
+      <div className="middle">
+        <p className="current-temp">69<span className="deg">&deg;</span></p>
+        <img className="weather-icon" src="./lib/assets/thunder-icon.svg" alt="weather condition icon"/>
+        <p className="weather-condition">Partly Cloudy</p>
+        <p className="hi-low">{highLow}</p>
+      </div>
+
+      <div className="bottom">
+        <p className="weather-summary">Windy, chance of a few late night showers. Lows overnight in the mid 50s.</p>
+      </div>
+
+    </aside>
+  )
+}
+
+export default AsideForecast;
