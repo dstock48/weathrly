90667afee9d13782493e849cbcb3ee0e4941d810
Dave W <dviper@gmail.com>
2017-05-28 23:03:25 -0600

    add search input component, update styles

--- lib/Weathrly/components/ForecastDetail/ForecastDetail.jsx
+++ lib/Weathrly/components/ForecastDetail/ForecastDetail.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DetailList from '../DetailList/DetailList';
+import SearchInput from '../SearchInput/SearchInput';
 import './ForecastDetail.css'
 
 function ForecastDetail() {
   return (
     <main className="ForecastDetail">
+      <SearchInput />
       <DetailList />
     </main>
   )
