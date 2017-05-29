7b5ee53ab38535eb73f34b09bf8708b0f2a93baf
Dave W <dviper@gmail.com>
2017-05-29 16:34:06 -0600

    update arrow fn to use explicit return

--- lib/Weathrly/components/SearchInput/SearchInput.jsx
+++ lib/Weathrly/components/SearchInput/SearchInput.jsx
@@ -2,13 +2,12 @@ import React from 'react'; // eslint-disable-line
 import './SearchInput.css';
 
 
-const SearchInput = () => {
-  return (
-    <section className="SearchInput">
-      <input className="search-input" type="text" placeholder="City / State / Zip"/><button className="search-btn"><img className="search-icon" src="lib/assets/magnifier.svg" alt=""/></button>
-    </section>
-  );
-};
+const SearchInput = () => (
+  <section className="SearchInput">
+    <input className="search-input" type="text" placeholder="City / State / Zip"/><button className="search-btn"><img className="search-icon" src="lib/assets/magnifier.svg" alt=""/></button>
+  </section>
+);
+
 
 export default SearchInput;
 
