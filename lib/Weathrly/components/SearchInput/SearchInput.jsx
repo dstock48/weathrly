faf3596193f59274e31211a2e9797b503b825d7f
Dave W <dviper@gmail.com>
2017-05-29 14:39:57 -0600

    convert fn to arrow fn

--- lib/Weathrly/components/SearchInput/SearchInput.jsx
+++ lib/Weathrly/components/SearchInput/SearchInput.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line
 import './SearchInput.css';
 
-function SearchInput() {
 
+const SearchInput = () => {
   return (
     <section className="SearchInput">
       <input className="search-input" type="text" placeholder="City / State / Zip"/><button className="search-btn"><img className="search-icon" src="lib/assets/magnifier.svg" alt=""/></button>
     </section>
   );
-}
+};
 
 export default SearchInput;
+
+// function SearchInput() {
+//
+//   return (
+//     <section className="SearchInput">
+//       <input className="search-input" type="text" placeholder="City / State / Zip"/><button className="search-btn"><img className="search-icon" src="lib/assets/magnifier.svg" alt=""/></button>
+//     </section>
+//   );
+// }
