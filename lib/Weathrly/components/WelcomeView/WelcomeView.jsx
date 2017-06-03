42c56c39033c6147e1b38bcdd9ff2f352bc5c8a0
Dave W <dviper@gmail.com>
2017-06-03 15:58:54 -0600

    add more clouds to welcomeView, adjust positioning of clouds

--- lib/Weathrly/components/WelcomeView/WelcomeView.jsx
+++ lib/Weathrly/components/WelcomeView/WelcomeView.jsx
@@ -8,6 +8,9 @@ function WelcomeView({ data, locationHandler }) {
     <img id="cloud1" className="clouds" src="lib/assets/clouds1.png"/>
     <img id="cloud2" className="clouds" src="lib/assets/clouds2.png"/>
     <img id="cloud3" className="clouds" src="lib/assets/clouds3.png"/>
+    <img id="cloud1" className="cloudsL" src="lib/assets/clouds1.png"/>
+    <img id="cloud2" className="cloudsL" src="lib/assets/clouds2.png"/>
+    <img id="cloud3" className="cloudsL" src="lib/assets/clouds3.png"/>
     <span className="emoji">😄</span>
     <h1 className="welcome-msg">Welcome!</h1>
     <p className="welcome-sub-msg">Enter your location</p>
