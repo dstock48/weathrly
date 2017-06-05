d74acab304aadb61e67659d57f2879396d204065
Juan Diego Jimenez <jdiejim@gmail.com>
2017-06-05 15:25:22 -0600

    add autocomplete search functionality

--- lib/Weathrly/components/ErrorView/ErrorView.jsx
+++ lib/Weathrly/components/ErrorView/ErrorView.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import SearchInput from '../SearchInput/SearchInput';
 import './ErrorView.css';
 
-function ErrorView({ data, locationHandler }) {
+function ErrorView({ data, locationHandler, trie }) {
   return (
     <section className="ErrorView">
       <span className="emoji">🌩 😭 🌩</span>
       <h1 className="error-msg">Not Found!</h1>
       <p className="error-sub-msg">Try again...</p>
       <SearchInput
+        trie={trie}
         errorClass={true}
         data={data}
         handler={locationHandler}
