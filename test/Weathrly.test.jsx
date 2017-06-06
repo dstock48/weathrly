adee00780869cd5b218841104e961c3f6cb07c0e
Dave W <dviper@gmail.com>
2017-06-06 14:33:34 -0600

    add tests for Weathrly component

---
+++ test/Weathrly.test.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable */
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Weathrly from '../lib/Weathrly/Weathrly';
+
+describe('Weathrly Component', () => {
+  const storageMock = () => {
+    const storage = {};
+    return {
+      location: 'oakland, ca',
+      clear() {
+        return this.location = '';
+      },
+      getItem(key) {
+        return key in storage ? storage[key] : null;
+      },
+    };
+  };
+
+  window.localStorage = storageMock();
+
+
+  it('should render welcome view to the page when no location is set', () => {
+    let WeathrlyComp = mount(<Weathrly />);
+    let welcomeHeading = <h1 className="welcome-msg">Welcome!</h1>;
+
+    expect(WeathrlyComp.contains(welcomeHeading)).toEqual(false);
+
+    localStorage.clear();
+    WeathrlyComp = mount(<Weathrly />);
+
+    expect(WeathrlyComp.contains(welcomeHeading)).toEqual(true);
+  });
+
+  it('should render standard view to the page when an acceptable location is set', () => {
+    let WeathrlyComp = mount(<Weathrly />);
+    let welcomeHeading = <h1 className="welcome-msg">Welcome!</h1>;
+
+    localStorage.clear();
+    expect(WeathrlyComp.contains(welcomeHeading)).toEqual(true);
+
+    localStorage.location = 'denver, co'
+    WeathrlyComp = mount(<Weathrly />);
+
+    expect(WeathrlyComp.contains( <section className="AsideForecast" /> )).toEqual(true);
+  });
+
+  it('should render error view to the page when location is not found', () => {
+    let WeathrlyComp = mount(<Weathrly />);
+    let errorHeading = <h1 className="error-msg">Not Found!</h1>;
+
+    expect(WeathrlyComp.state().isNotFound).toEqual(false)
+    expect(WeathrlyComp.contains( errorHeading )).toEqual(false);
+
+    WeathrlyComp.setState({isNotFound: true})
+
+    expect(WeathrlyComp.state().isNotFound).toEqual(true)
+    expect(WeathrlyComp.contains( errorHeading )).toEqual(true);
+
+  });
+});
