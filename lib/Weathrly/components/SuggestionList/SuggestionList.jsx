382260ddd908b1aaa534d1d98f73db85a5e9b1b3
Dave W <dviper@gmail.com>
2017-06-11 15:38:50 -0600

    small changes, start getting ready for Heroku deploy

--- lib/Weathrly/components/SuggestionList/SuggestionList.jsx
+++ lib/Weathrly/components/SuggestionList/SuggestionList.jsx
@@ -4,20 +4,20 @@ import './SuggestionList.css';
 
 function SuggestionList({ suggestions, selectHandler }) {
   const suggestionsArray = suggestions.slice(0, 5).map(e =>
-//    <option value={e} key={getRandomKey()} className="suggestion" />
-    <li key={getRandomKey()} className="suggestion" onClick={() => selectHandler(e)}>
-      <button className="suggestion-btn">{e}</button>
-    </li>
+    <option value={e} key={getRandomKey()} className="suggestion" />
+//    <li key={getRandomKey()} className="suggestion" onClick={() => selectHandler(e)}>
+//      <button className="suggestion-btn">{e}</button>
+//    </li>
   );
 
   return (
-    <section id="SuggestionList" className="SuggestionList">
-      {suggestionsArray}
-    </section>
+//    <section id="SuggestionList" className="SuggestionList">
+//      {suggestionsArray}
+//    </section>
 
-  //  <datalist id="SuggestionList" className="SuggestionList">
-  //    {suggestionsArray}
-  //  </datalist>
+    <datalist id="SuggestionList" className="SuggestionList">
+      {suggestionsArray}
+    </datalist>
   );
 }
 
